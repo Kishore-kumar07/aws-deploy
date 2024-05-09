49bcf75080042a65cdabc94e3d98a203a5a19e94
kishore <kishoregopi1540@gmail.com>
2024-05-09 09:57:09 +0530

    model added and prediction working

--- prediction/urls.py
+++ prediction/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,predict_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('',index),
+    path('predict/',predict_view)
+
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pickle
+import pandas as pd
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def predict_view(request):
+    if request.method == 'POST':
+        gender = int(request.POST.get('gender'))
+        age = int(request.POST.get('age'))
+        height = float(request.POST.get('height'))
+        weight = float(request.POST.get('weight'))
+        duration = float(request.POST.get('duration'))
+        heart_rate = float(request.POST.get('heart_rate'))
+        body_temp = float(request.POST.get('body_temp'))
+
+        # Create a DataFrame with the input data
+        input_data = pd.DataFrame({
+            'Gender': [gender],
+            'Age': [age],
+            'Height': [height],
+            'Weight': [weight],
+            'Duration': [duration],
+            'Heart_Rate': [heart_rate],
+            'Body_Temp': [body_temp]
+        })
+
+        # Load the model from the .pkl file
+        model = pickle.load(open('templates/model.pkl','rb'))
+
+        # Use the model to make predictions
+        prediction = model.predict(input_data)
+
+        # Handle the prediction as needed (e.g., return it as an HTTP response)
+        return HttpResponse(f'Prediction: {prediction}')
+    else:
+        # Render the form template if it's a GET request
+        return render(request, 'index.html')
